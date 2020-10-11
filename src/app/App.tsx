2c777aff347162a3c266e05721bd0df2d8883c4e
Ritik Mishra <ritik.mishra314@gmail.com>
2020-10-10 19:56:16 -0500

    add navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import {FileViewingPane} from "./components/file-viewing-pane/file-viewing-pane";
+import {CustomNavbar} from "./components/navbar/navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <FileViewingPane/>
+    <div>
+      <CustomNavbar/>
+      <FileViewingPane/>
+    </div>
   );
 }
 
