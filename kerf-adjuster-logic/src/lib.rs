0082032caeac19f8a833181c034d3e3263c29d71
Ritik Mishra <ritik.mishra314@gmail.com>
2021-02-27 02:48:28 -0600

    improve contour combination
    it would previously infinitely loop if there was an open contour

--- kerf-adjuster-logic/src/lib.rs
+++ kerf-adjuster-logic/src/lib.rs
@@ -3,14 +3,65 @@ use wasm_bindgen::prelude::*;
 use console_error_panic_hook;
 use dxf::Drawing;
 use std::io::BufReader;
-use std::{
-    collections::HashMap,
-};
 pub mod contour;
 use contour::{Contour, ContourVecToDxf};
 pub mod errors;
 
-fn drawing_to_contours(drawing: Drawing) -> HashMap<usize, Contour> {
+/// Merges the `head` contour with one of the `tail` contours if possible
+/// 
+/// If the head contour X can be combined with one of the tail contours Y
+///     Then it returns a vector containing the combination of X and Y, as well as the remaining uncombined tail contours
+/// 
+/// If the head contour X cannot be combined with any tail contour
+///     Then it returns None
+fn collapse_contours_once(head: &Contour, tail: &[Contour]) -> Option<Vec<Contour>> {
+    for (i, contour) in tail.iter().enumerate() {
+        if let Ok(combined) = head
+            .clone()
+            .combine_attempt(contour.clone())
+        {
+            let mut ret = tail.to_vec();
+            ret.remove(i);
+            ret.push(combined);
+
+            return Some(ret);
+        }
+    }
+    return None;
+}
+
+
+/// Maximizes the number of closed contours in a contour list by combining them
+/// 
+/// e.g If we have a vector of 4 open contours, where each one is the side of a rectangle, 
+/// it will return a vector of 1 closed contour where the  
+fn collapse_contours(mut contours: Vec<Contour>) -> Vec<Contour> {
+    let mut final_contours = Vec::new();
+
+    loop {
+        // Go through the contours
+        match contours.as_slice() {
+            // Stop if there are no contours left to combine
+            [] => break, 
+
+            // Separate the first contour from the remaining ones
+            [head, tail @ ..] => {
+                match collapse_contours_once(head, tail) {
+                    // If we couldn't combine it, `head` must be a complete contour
+                    None => {
+                        final_contours.push(head.clone());
+                        contours = tail.to_vec(); // remove head from contours list
+                    },
+                    Some(new_contours) => contours = new_contours
+                }
+            }
+        }
+    }
+
+    return final_contours;
+}
+
+fn drawing_to_contours(drawing: Drawing) -> Vec<Contour> {
     // Convert each DXF entity (arc, circle, text, etc) into a "Contour" which can be more easily manipulated by us
     let contours = drawing
         .entities
@@ -20,45 +71,11 @@ fn drawing_to_contours(drawing: Drawing) -> HashMap<usize, Contour> {
         .collect::<Vec<_>>();
 
     // Partition the contours by whether or not they are open (i.e can be joined to another contour)
-    let (mut contours, mut finished_contours) = contours
+    let (contours, mut finished_contours) = contours
         .into_iter()
-        .enumerate()
-        .partition::<HashMap<usize, _>, _>(|(_, c)| c.is_open());
+        .partition::<Vec<_>, _>(|c| c.is_open());
 
-    // Group the contours with each other.end_points.is_some()
-    // TODO: handle unclosed contours
-    while contours.len() > 0 {
-        'each_a: for (i, current_contour) in contours.iter() {
-            loop {
-                for (j, other_contour) in contours.iter() {
-                    let (i, j) = (*i, *j);
-                    if i != j {
-                        if let Ok(combined) = current_contour
-                            .clone()
-                            .combine_attempt(other_contour.clone())
-                        {
-                            // We will combine other_contour into this_contour
-                            // Remove other_contour from contours map
-                            contours.remove(&j);
-
-                            // Check if the combined result is open or closed, and
-                            // put it in its place accordingly
-                            if combined.end_points.is_some() {
-                                contours.insert(i, combined);
-                            } else {
-                                contours.remove(&i);
-                                finished_contours.insert(i, combined);
-                            }
-
-                            // Restart the contour merging process from the beginning
-                            // helps with ensuring consistent behavior
-                            break 'each_a;
-                        }
-                    }
-                }
-            }
-        }
-    }
+    finished_contours.extend(collapse_contours(contours));
 
     return finished_contours;
 }
@@ -72,7 +89,7 @@ pub fn offset_drawing(drawing_bytes: &[u8], offset_amount: f64) -> Vec<u8> {
     // offset the contours
     let new_drawing = drawing_contours
         .into_iter()
-        .map(|(_, c)| c.offset_contour(offset_amount).unwrap_or(c))
+        .map(|c| c.offset_contour(offset_amount).unwrap_or(c))
         .collect::<Vec<_>>()
         .to_dxf();
 
